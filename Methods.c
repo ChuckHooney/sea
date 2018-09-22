a55fd2b9c4f20474d828384afe2ba3ef38c8fe72
Raghav Narula <raghavnarula@Raghavs-Air.home>
2018-09-21 17:36:26 -0700

    More Methods

--- Methods.c
+++ Methods.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 
-void play (int x, int y){
+void add (int x, int y){
   printf ("We are Venom\n") ;
   printf ("%d\n", x) ;
   printf ("%d\n", y) ;
@@ -12,6 +12,39 @@ void play (int x, int y){
   printf ("%d\n", z) ;
 }
 
+void subtract (int x, int y) {
+  printf ("I am Carnage\n") ;
+  printf ("%d\n", x) ;
+  printf ("%d\n", y) ;
+
+  int z ;
+  z = x - y ;
+  printf ("%d\n", z) ;
+}
+
+void divide (float x, float y) {
+  printf ("Toxinnnnnnnnn\n") ;
+  printf ("%.3f\n", x) ;
+  printf ("%.3f\n", y) ;
+
+  float z ;
+  z = x / (float)y ;
+  printf ("%.3f\n", z) ;
+}
+
+void multiply (int x, int y) {
+  printf ("Anti-Venom, Destroy!\n") ;
+  printf ("%d\n", x) ;
+  printf ("%d\n", y) ;
+
+  int z ;
+  z = x * y ;
+  printf ("%d\n", z) ;
+}
+
 int main() {
-  play(11,13) ;
+  add (11,-13) ;
+  subtract (11,13 ) ;
+  divide (-10.5, 3.1) ;
+  multiply (2, 5) ;
 }
