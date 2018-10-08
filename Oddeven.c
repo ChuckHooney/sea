48d8f0f296f446bc2fd810143408005cc6af1f77
Raghav Narula <raghavnarula@Raghavs-Air.home>
2018-10-08 16:53:45 -0700

    new method

---
+++ Oddeven.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void OddEven (int x){
+  int z ;
+  z = x % 2 ;
+
+  if (z == 0){
+    printf ("%d is even\n", x) ;
+  }
+  else if (z == 1){
+    printf ("%d is odd\n", x) ;
+  }  
+}
+
+int main (){
+  OddEven (11) ;
+  OddEven (32) ;
+  OddEven (0) ;
+  OddEven (1) ;
+  OddEven (2) ;
+}
