c4bb2776548110b16687184d9c8e073a7fc0cd75
Raghav Narula <raghavnarula@Raghavs-Air.home>
2018-10-19 18:22:42 -0700

    to be continued

--- tablemethod.c
+++ tablemethod.c
@@ -2,12 +2,14 @@
 
 void table (int x,int y) {
 
-  for (int m=1; m<=13; m++) {
-      printf ("17 * %d =%d\n", m, m*17) ;
+  for (int m=x; m<=50; m++, y++) {
+      printf ("%d * %d =%d\n", x, y, x*y) ;
   }
 }
 
 
 int main (){
   table (5,2) ;
+  table (5,3) ;
+  table (5,4) ;
 }
